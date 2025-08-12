237a84ba597e93854ed5f08f07b52d8b29fd8520
Rehabalnairi <133367687+Rehabalnairi@users.noreply.github.com>
2025-08-12 09:41:53 +0400

    add Required to Airpot class

--- DomainModels.cs
+++ DomainModels.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,16 +9,25 @@ namespace Flight_Management_Company
 {
     public class Airpot
     {
+        [Key]
         public int AirportId { get; set; }
+        [Required,StringLength(3)]
         public string IATA { get; set; }
+        [Required]
+        public String Name { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
+        [Required]
         public string TimeZone { get; set; }
+        public ICollection<Route> OriginRoutes { get; set; }
+        public ICollection<Route> DestinationRoutes { get; set; }
     }
 
     public class Aircraft
     {
+        [Key]
         public int AircraftId { get; set; }
+        [Required]
         public string TailNumber { get; set; }
         public string Model { get; set; }
         public int Capacity { get; set; }
@@ -26,6 +36,7 @@ namespace Flight_Management_Company
 
     public class CrewMember
     {
+        [Key]
         public int CrewId { get; set; }
         public string FullName { get; set; }
         public string Role { get; set; } // e.g., Pilot, Flight Attendant
@@ -33,6 +44,7 @@ namespace Flight_Management_Company
     }
     public class Route
     {
+        [Key]
         public int RouteId { get; set; }
         public int DistanceKm { get; set; }
 
@@ -40,6 +52,7 @@ namespace Flight_Management_Company
 
     public class Flight
     {
+        [Key]
         public int FlightId { get; set; }
         public string FlightNumber { get; set; }
         public DateTime DepartureUtc { get; set; }
@@ -48,8 +61,10 @@ namespace Flight_Management_Company
     }
     public class Passenger
     {
+        [Key]
         public int PassengerId { get; set; }
         public string FullName { get; set; }
+        [Required]
         public string PassportNo { get; set; }
         public string Nationality { get; set; }
         public DateTime DOB { get; set; }
@@ -57,7 +72,9 @@ namespace Flight_Management_Company
 
     public class booking
     {
+        [Key]
         public int BookingId { get; set; }
+        [Required]
         public string BookingRef { get; set; }
         public DateTime BookingDate { get; set; }
         public string status { get; set; } // e.g., Confirmed, Cancelled
@@ -65,6 +82,7 @@ namespace Flight_Management_Company
     }
     public class Ticket
     {
+        [Key]
         public int TicketId { get; set; }
         public string SeatNumber { get; set; }
         public decimal Fare { get; set; }
@@ -72,6 +90,7 @@ namespace Flight_Management_Company
     }
     public class Baggage
     {
+        [Key]
         public int BaggageId { get; set; }
         public decimal WeightKg { get; set; }
         public string TagNumber { get; set; } // e.g., Checked-in, In Transit, Delivered
@@ -79,6 +98,7 @@ namespace Flight_Management_Company
 
     public class AircraftMaintenance
     {
+        [Key]
         public int MaintenanceId { get; set; }
         public DateTime MaintenanceDate { get; set; }
         public string Type { get; set; }

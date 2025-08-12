592791793d53d77c9ba74469c54926fb37949aad
Rehabalnairi <133367687+Rehabalnairi@users.noreply.github.com>
2025-08-12 09:45:33 +0400

    add ICollection for class CrewMember

--- DomainModels.cs
+++ DomainModels.cs
@@ -40,9 +40,12 @@ namespace Flight_Management_Company
     {
         [Key]
         public int CrewId { get; set; }
+        [Required]
         public string FullName { get; set; }
+        [Required]
         public string Role { get; set; } // e.g., Pilot, Flight Attendant
         public string LicenseNo { get; set; }
+         public ICollection<FlightCrew> flightCrews { get; set; } // Flights this crew member is assigned to
     }
     public class Route
     {

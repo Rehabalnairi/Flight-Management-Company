3128b309b0d191ade9a46071c57858ffa67b5cd1
Rehabalnairi <133367687+Rehabalnairi@users.noreply.github.com>
2025-08-12 09:46:58 +0400

    add Required to Route class

--- DomainModels.cs
+++ DomainModels.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -52,6 +53,17 @@ namespace Flight_Management_Company
         [Key]
         public int RouteId { get; set; }
         public int DistanceKm { get; set; }
+        [Required]
+        public int OriginAirportId { get; set; }
+        [ForeignKey("OriginAirportId")]
+        public Airpot OriginAirport { get; set; }
+
+        [Required]
+        public int DestinationAirportId { get; set; }
+        [ForeignKey("DestinationAirportId")]
+        public Airpot DestinationAirport { get; set; }
+
+        public ICollection<Flight> Flights { get; set; }
 
     }
 

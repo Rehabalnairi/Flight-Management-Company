2ccc8dbf320a493646c85f2f071c9a64d626ea5d
Rehabalnairi <133367687+Rehabalnairi@users.noreply.github.com>
2025-08-13 11:51:33 +0400

    add At least 150 pieces of baggage associated to tickets.

--- Service/BaggageService.cs
+++ Service/BaggageService.cs
@@ -1,18 +1,42 @@
-﻿using System;
+﻿using Flight_Management_Company.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Flight_Management_Company.Service
 {
-   public  class BaggageService
+    public class BaggageService
     {
         private readonly FlightContext _flightContext;
+
         public BaggageService(FlightContext flightContext)
         {
             _flightContext = flightContext;
+        }
 
+        public void SeedBaggages()
+        {
+            if (_flightContext.Baggages.Any()) return;
+
+            var random = new Random();
+            var tickets = _flightContext.Tickets.ToList();
+
+            var baggages = new List<Baggage>();
+
+            for (int i = 1; i <= 150; i++)
+            {
+                var ticket = tickets[random.Next(tickets.Count)];
+
+                baggages.Add(new Baggage
+                {
+                    TicketId = ticket.TicketId,
+                    WeightKg = Math.Round((decimal)(random.NextDouble() * 40 + 5), 2),// between 5 and 45
+                    TagNumber = "BG" + i.ToString("D4")
+                });
+            }
+
+            _flightContext.Baggages.AddRange(baggages);
+            _flightContext.SaveChanges();
         }
     }
 }

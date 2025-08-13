ab022410e2de84853edf3fb51de2da6b216a5e7a
Rehabalnairi <133367687+Rehabalnairi@users.noreply.github.com>
2025-08-13 12:41:15 +0400

    create DTO class

---
+++ DTOs.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flight_Management_Company
+{
+    public class CrewDto
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+    }
+
+    public class FlightManifestDto
+    {
+        public string FlightNumber { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public DateTime DepUtc { get; set; }
+        public DateTime ArrUtc { get; set; }
+        public string AircraftTail { get; set; }
+        public int PassengerCount { get; set; }
+        public decimal TotalBaggageKg { get; set; }
+        public List<CrewDto> Crew { get; set; }
+    }
+    public class RouteRevenueDto
+    {
+        public int RouteId { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public decimal Revenue { get; set; }
+        public int SeatsSold { get; set; }
+        public decimal AvgFare { get; set; }
+    }
+    public class CrewConflictDto
+    {
+        public int CrewId { get; set; }
+        public string CrewName { get; set; }
+        public int FlightAId { get; set; }
+        public int FlightBId { get; set; }
+        public DateTime FlightADep { get; set; }
+        public DateTime FlightBDep { get; set; }
+    }
+ 
+
+}
